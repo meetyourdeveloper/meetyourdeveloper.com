e572352288f90948420a4e2bdb41258616567cfe
Jarrod Taylor <jarrodtaylor@icloud.com>
2016-08-20 22:31:20 -0400

    Adds ex_doc

--- mix.exs
+++ mix.exs
@@ -3,11 +3,16 @@ defmodule MeetYourDeveloper.Mixfile do
 
   def project do
     [app: :meet_your_developer,
+     name: "Meet Your Developer",
+     description: "Teaching software developers to market themselves effectively.",
+     source_url: "https://github.com/meetyourdeveloper/meetyourdeveloper.github.io",
+     homepage_url: "http://meetyourdeveloper.com",
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     docs: [main: "readme", extras: ["README.md"]]]
   end
 
   def application do
@@ -18,6 +23,7 @@ defmodule MeetYourDeveloper.Mixfile do
   defp deps do
     [{:cowboy,    "~> 1.0.0"},
      {:earmark,   "~> 1.0.1"},
+     {:ex_doc,    "~> 0.12"},
      {:exfswatch, "~> 0.1.0"},
      {:plug,      "~> 1.0"}]
   end
